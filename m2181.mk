7cf205a1718cfa15fbd2c7d5145079325534ed57
TRANSAERO21 <martynenkodima21@gmail.com>
2021-09-06 21:58:15 +0300

    m2181: Correct boot hal package name

--- m2181.mk
+++ m2181.mk
@@ -23,8 +23,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mock.recovery
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
 # Boot HAL
 PRODUCT_PACKAGES += \
